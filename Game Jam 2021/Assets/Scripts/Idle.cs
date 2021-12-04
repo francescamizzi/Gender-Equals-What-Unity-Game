a07e7d0350b17ec9bd7c6ae428ef37bb9d38f504
britneyv <81623990+britneyv@users.noreply.github.com>
2021-12-04 18:15:22 +0100

    roomate finding

---
+++ Game Jam 2021/Assets/Scripts/Idle.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Idle : StateMachineBehaviour
+{
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+        animator.SetBool("isWandering", true);
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //Instantiate(effect, animator.transform.position, Quaternion.identity);
+    }
+}
---
+++ Game Jam 2021/Assets/Scripts/WanderingBehaviour.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderingBehaviour : StateMachineBehaviour
+{
+    private PatrolSpots patrol;
+    public float speed = 3;
+    private int randomSpot;
+
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        patrol = GameObject.FindGameObjectWithTag("PatrolSpots").GetComponent<PatrolSpots>();
+        randomSpot = Random.Range(0, patrol.patrolPoints.Length);
+    }
+
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (Vector2.Distance(animator.transform.position, patrol.patrolPoints[randomSpot].position) > 0.2f)
+        {
+            animator.transform.position = Vector2.MoveTowards(animator.transform.position, patrol.patrolPoints[randomSpot].position, speed * Time.deltaTime);
+
+        }
+        else
+        {
+            randomSpot = Random.Range(0, patrol.patrolPoints.Length);
+        }
+    }
+
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+}
