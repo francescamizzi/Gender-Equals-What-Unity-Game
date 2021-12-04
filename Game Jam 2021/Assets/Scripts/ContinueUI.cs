a69cb2abe1739fd77d5d41c7119646304ea1911b
Luke D'Amato <luke.damato.20@um.edu.mt>
2021-12-04 21:11:54 +0100

    intro UI, pause screen and task complete

---
+++ Game Jam 2021/Assets/Scripts/ContinueUI.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ContinueUI : MonoBehaviour
+{
+    //public ItemSlot TaskFinished;
+    public void Start(){
+      //  TaskFinished = GetComponent<ItemSlot>();
+    }
+
+
+    public void QuitGame()
+    {
+        Debug.Log("Returning home...");
+
+        // if(TaskFinished.TaskisFinished)
+        // {
+        //     Debug.Log("worked");
+        //     SceneManager.LoadScene("Fran Test");
+
+        // } 
+
+        // name of scene....
+    }
+}
