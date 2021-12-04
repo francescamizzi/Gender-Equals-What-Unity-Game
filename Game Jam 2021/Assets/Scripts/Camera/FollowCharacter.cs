a07e216fc7d733d1b7c5810c8d26b08493d0b19f
francescamizzi <72987846+francescamizzi@users.noreply.github.com>
2021-12-04 01:12:28 +0100

    player movement, camera tracking and basic collision

---
+++ Game Jam 2021/Assets/Scripts/Camera/FollowCharacter.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCharacter : MonoBehaviour
+{
+    private Transform player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!player)
+        {
+            player = GameObject.FindWithTag("Player").transform;
+        }
+    }
+
+    public void SetPlayer(GameObject p)
+    {
+        this.player = p.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!player)
+        {
+            player = GameObject.FindWithTag("Player").transform;
+        }
+        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+    }
+}
